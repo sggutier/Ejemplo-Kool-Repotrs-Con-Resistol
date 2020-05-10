b8d6954c1f8fd3d35032e05c8bd47eb2363753d3
Saál Germán Gutiérrez Calderón <me@sggc.me>
2020-05-10 17:17:38 -0500

    Añadido scrapper

---
+++ scrapper/db.sql
@@ -0,0 +1,30 @@
+drop database if exists coronachan;
+create database coronachan;
+use coronachan;
+
+DROP TABLE IF EXISTS `paises`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paises` (
+  `clave_pais` varchar(60) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  PRIMARY KEY (`clave_pais`),
+  KEY `indice_nombre` (`nombre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `registro_dia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registro_dia` (
+  `id_dia` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` datetime NOT NULL,
+  `confirmados` bigint(20) NOT NULL,
+  `muertes` bigint(20) NOT NULL,
+  `recuperados` bigint(20) NOT NULL,
+  `clave_pais` varchar(60) NOT NULL,
+  PRIMARY KEY (`id_dia`),
+  KEY `fk_clave_pais` (`clave_pais`),
+  CONSTRAINT `fk_clave_pais` FOREIGN KEY (`clave_pais`) REFERENCES `paises` (`clave_pais`)
+) ENGINE=InnoDB AUTO_INCREMENT=30016 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
---
+++ scrapper/scrapper.py
@@ -0,0 +1,76 @@
+import mysql.connector
+import requests
+from datetime import datetime
+
+
+def obtenConexion():
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        passwd="root",
+        database='coronachan'
+    )
+
+
+def obtenPaises():
+    return requests.get(url='https://api.covid19api.com/countries').json()
+
+
+def obtenPais(nombrePais):
+    strPais = 'https://api.covid19api.com/total/country/%s' % nombrePais
+    return requests.get(url = strPais).json()
+
+
+def insertaPaises():
+    print('Obten paises')
+    paises = obtenPaises()
+    print('Paises obtenidos')
+    strPaises = "insert into paises (clave_pais, nombre) values (%s, %s)"
+    db = obtenConexion()
+    for pais in paises:
+        clave = pais['Slug']
+        nombre = pais['Country']
+        print(clave, nombre)
+        c = db.cursor()
+        c.execute(strPaises, (clave, nombre))
+        db.commit()
+    db.close()
+
+
+def insertaDatosPorPais():
+    print('Obten paises')
+    paises = obtenPaises()
+    print('Paises obtenidos')
+    db = obtenConexion()
+    paises = obtenPaises()
+    formato = "%Y-%m-%dT%H:%M:%SZ"
+    strRegistro = 'insert into registro_dia (fecha, confirmados, muertes, recuperados, clave_pais) values (%s, %s, %s, %s, %s)'
+    for pais in paises:
+        slug = pais['Slug']
+        print('Obteniendo datos de pais %s' % slug)
+        dias = obtenPais(slug)
+        print('Datos de %s obtenidos' % slug)
+        for dia in dias:
+            confirmados = dia['Confirmed']
+            muertes = dia['Deaths']
+            recuperados = dia['Recovered']
+            fecha = datetime.strptime(dia['Date'], formato)
+            c = db.cursor()
+            c.execute(strRegistro, (
+                str(fecha),
+                confirmados,
+                muertes,
+                recuperados,
+                slug
+            ))
+            db.commit()
+    db.close()
+
+
+def main():
+    insertaPaises()
+    insertaDatosPorPais()
+
+
+if __name__ == '__main__':
+    main()
